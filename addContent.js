5b0e8af43ea25c7df088c8b75e4e34cda6e8baa3
MinhaoPixelForce <minhao.zhu@pixelforce.com.au>
2023-01-19 10:58:42 +1030

    Add content javascript

---
+++ addContent.js
@@ -0,0 +1,24 @@
+document.body.onload = addElement;
+
+function addElement() {
+  // create a new div element
+    const newDivStyle = {
+        color: "white",
+        "background-color": "red",
+        "font-size": "40px",
+        "position": "fixed",
+        "top": "0px",
+        "right": "0px"
+    }
+    const newDiv = document.createElement("div");
+    newDiv.style = newDivStyle;
+    // and give it some content
+    const newContent = document.createTextNode("Hi there and greetings!");
+
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+}
